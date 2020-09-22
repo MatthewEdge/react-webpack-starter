30c42c258321edbd186bebda01cc41f2dbf5bf8d
Matthew Edge <medge@medgelabs.io>
2020-09-22 10:23:14 -0400

    Add alias for app root

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,24 @@ module.exports = {
   devtool: "cheap-eval-source-map",
   entry: {
     app: [path.join(__dirname, "src", "index.js")],
-    vendors: ["react", "react-dom", "react-router"]
+    vendors: ["react", "react-dom", "react-router"],
   },
+
   output: {
     path: path.join(__dirname, "public"),
     filename: "[name].[hash].bundle.js",
     sourceMapFilename: "[name].[hash].bundle.map",
-    publicPath: '/',
+    publicPath: "/",
   },
+
   resolve: {
     extensions: [".js", ".jsx", ".css", ".scss"],
     alias: {
-      "@styles": path.resolve(__dirname, "src", "scss")
-    }
+      "@styles": path.resolve(__dirname, "src", "scss"),
+      "@app": path.resolve(__dirname, "src"),
+    },
   },
+
   module: {
     rules: [
       {
@@ -30,58 +34,59 @@ module.exports = {
           {
             loader: "babel-loader",
             options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"]
-            }
-          }
-        ]
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
       },
+
       {
         test: /\.css$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              hmr: process.env.NODE_ENV === 'development',
+              hmr: process.env.NODE_ENV === "development",
             },
           },
-          'css-loader',
-        ]
+          "css-loader",
+        ],
       },
+
       // Assets loaders
       {
         test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
-        use: [
-          { loader: "url-loader?limit=10000" }
-        ]
+        use: [{ loader: "url-loader?limit=10000" }],
       },
       {
         test: /\.(gif|jpg|png)/,
         exclude: /(node_modules|bower_components)/,
-        use: [
-          { loader: "url-loader?limit=10000" }
-        ]
-      }
-    ]
+        use: [{ loader: "url-loader?limit=10000" }],
+      },
+    ],
   },
+
   plugins: [
     new PrettierPlugin({
       printWidth: 120,
       tabWidth: 2,
       useTabs: false,
       semi: false,
-      encoding: 'utf-8',
+      encoding: "utf-8",
       bracketSpacing: true,
-      endOfLine: 'lf',
+      endOfLine: "lf",
     }),
+
     new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].css',
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].css",
       ignoreOrder: false,
     }),
+
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.join(__dirname, "src", "template.html"),
-      inject: "body"
-    })
-  ]
+      inject: "body",
+    }),
+  ],
 }
